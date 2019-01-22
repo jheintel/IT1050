257a306ea3747221cd83924a2af7038ca3719eb0
Jonathan Heintel <darthpeach@gmail.com>
2019-01-22 10:21:32 -0500

    declaing vars changed

---
+++ Declaring_Vars/Declaring_Vars_soln/Declaring_Vars_proj/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Declaring_Vars_proj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //comment
+            string name;
+            int age = 0;
+            bool afraidOfGod;
+            name = "Elizabeth Comstock";
+            age = 20;
+            afraidOfGod = true;
+        }
+
+    }
+}
