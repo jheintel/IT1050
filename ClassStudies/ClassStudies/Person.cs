fabdc1f46da01bddd3fe71b0d716478ceeff8bdb
Jonathan Heintel <darthpeach@gmail.com>
2019-02-05 10:12:23 -0500

    added ClassStudies project

---
+++ ClassStudies/ClassStudies/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassStudies
+{
+    class Person
+    {
+        // Member variable declaration (Instance variable)
+
+        public string Name;
+        public int Age;
+    }
+}
---
+++ ClassStudies/ClassStudies/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ClassStudies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Jon = new Person();
+            Jon.Age = 29;
+            Jon.Name = "Jon";
+
+
+            System.Console.Write(Jon.Age);
+            System.Console.ReadKey();
+        }
+    }
+}
