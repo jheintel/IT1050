e2d11e306a97ca5266de76ddbfafc8d364557697
Jonathan Heintel <darthpeach@gmail.com>
2019-02-05 20:38:00 -0500

    move some stuff around, i dont know

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -6,6 +6,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            const int INCHES_PER_FOOT = 12;
+            const double INCHES_TO_CM = 2.54;
+
             string firstName,
                    middleInitial,
                    LastName,
@@ -20,11 +23,11 @@ namespace ConsoleApp1
 
             // Asks user for a name then truncates the answers into fullName
             {
-                Console.Write("What is your first name?: ");
+                Console.Write("What is your first name?        : ");
                 firstName = Console.ReadLine();
-                Console.Write("What is your middle initial?: ");
+                Console.Write("What is your middle initial?    : ");
                 middleInitial = Console.ReadLine() + ".";
-                Console.Write("What is your last name?: ");
+                Console.Write("What is your last name?         : ");
                 LastName = Console.ReadLine();
 
                 fullName = firstName + " " + middleInitial + " " + LastName;
@@ -33,22 +36,22 @@ namespace ConsoleApp1
             /* Asks for the users height in feet and inches then converts it to some voodoo number
             that is yet to be understood by our engineers. */
             {
-                Console.Write("How many feet tall are you (excluding inches using numerals)?: ");
+                Console.Write("How many feet tall are you \n" +
+                              "excluding inches using numerals?: ");
                 heightFeet = int.Parse(Console.ReadLine());
-                Console.Write("and how many inches beyond that?(Decimal): ");
+                Console.Write("and how many inches beyond that?: ");
                 heightInches = double.Parse(Console.ReadLine());
 
-                totalHeightCM = (heightFeet * 12 + heightInches) * 2.54;
+                totalHeightCM = (heightFeet * INCHES_PER_FOOT + heightInches) * INCHES_TO_CM;
             }
 
             // Some personal questions for border security reasons
             {
-                Console.Write("How old are you?: ");
+                Console.Write("How old are you?                : ");
                 age = int.Parse(Console.ReadLine());
-
-                Console.Write("Are you a citizen y or n?: ");
-                isCitizenAnswer = Console.ReadLine();
-                isCitizen = isCitizenAnswer.Contains("y") || isCitizenAnswer.Contains("Y");
+                Console.Write("Are you a citizen y or n?       : ");
+                isCitizenAnswer = Console.ReadLine().ToLower();
+                isCitizen = isCitizenAnswer.Contains("y") || isCitizenAnswer.Contains("t");
 
                 canVote = isCitizen && (age >= 18);
             }
@@ -56,7 +59,6 @@ namespace ConsoleApp1
             // Print the whole mess out and hope the client doesn't look to closely at our methods
             {
                 Console.WriteLine();
-
                 Console.Write("So... \n" + "Your name is " + fullName + "\n" +
                               "You are " + totalHeightCM + "cm tall \n");
                 if (canVote)
