5c99ddd5fa21ab2083e8ddf32a7567fad107ab95
Jonathan Heintel <darthpeach@gmail.com>
2019-05-16 15:54:19 -0400

    added SubClasses

---
+++ SubClasses/SubClasses/ManMadeSat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SubClasses
+{
+    class ManMadeSat : Satellite
+    {
+        public ManMadeSat(string primary, string name, string creator)
+        {
+            Name = name;
+            Primary = primary;
+            Creator = creator;
+        }
+
+        public string Creator { get; set; }
+
+        public override void PrintSystem()
+        {
+            base.PrintSystem();
+            Console.WriteLine("Created by: " + Creator);
+        }
+    }
+}
---
+++ SubClasses/SubClasses/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SubClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Satellite moon = new Satellite("Earth", "Moon");
+            ManMadeSat station = new ManMadeSat("Earth", "The ISS", "Nasa");
+
+            moon.Name = "The Moon";
+
+            moon.PrintSystem();
+            Console.WriteLine();
+            station.PrintSystem();
+            Console.ReadKey();
+        }
+    }
+}
