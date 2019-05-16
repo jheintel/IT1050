5628ea5511e1e4d67da7c7350a30f501a39a52da
Jonathan Heintel <darthpeach@gmail.com>
2019-05-16 16:13:19 -0400

    minor experiments

--- SubClasses/SubClasses/Program.cs
+++ SubClasses/SubClasses/Program.cs
@@ -9,9 +9,19 @@ namespace SubClasses
             Satellite moon = new Satellite("Earth", "Moon");
             ManMadeSat station = new ManMadeSat("Earth", "The ISS", "Nasa");
 
+
+            // Object Initializer doesn't call a constructor to set values
+            Satellite europa = new Satellite()
+            {
+                Name = "Europa",
+                Primary = "Jupiter"
+            };
+
             moon.Name = "The Moon";
 
             moon.PrintSystem();
+            Console.WriteLine();
+            europa.PrintSystem();
             Console.WriteLine();
             station.PrintSystem();
             Console.ReadKey();
